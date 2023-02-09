5e774d78f7688b74dc8fc24436d600f2466347f0
Ricardo <ricardinhogiasson16@gmail.com>
2023-02-09 06:57:05 -0300

    Atualizando Dados do cliente

--- bantads-cliente/src/main/java/com/bantads/cliente/services/rabbitmq/RabbitMQConsumer.java
+++ bantads-cliente/src/main/java/com/bantads/cliente/services/rabbitmq/RabbitMQConsumer.java
@@ -26,6 +26,9 @@ public class RabbitMQConsumer {
     private RabbitTemplate rabbitTemplate;
     public static final String FILA_REGISTRO_CLIENTE = "FILA_REGISTRO_CLIENTE";
     public static final String FILA_ERRO_NOVO_CLIENTE = "FILA_ERRO_NOVO_CLIENTE";
+
+    public static final String FILA_UPDATE_CLIENTE = "FILA_UPDATE_CLIENTE";
+    public static final String FILA_ERRO_UPDATE_CLIENTE = "FILA_ERRO_UPDATE_CLIENTE";
     public static final String FILA_ERRO_NOVO_CLIENTE_AUTENTICACAO = "FILA_ERRO_NOVO_CLIENTE_AUTENTICACAO";
     @Autowired
     private ObjectMapper objectMapper;
@@ -59,10 +62,45 @@ public class RabbitMQConsumer {
             // ROLLBACK
             System.out.println("Erro ao salvar cliente (" + cliente.getNome() + ") " + msg);
             var id_erro = objectMapper.writeValueAsString(cliente.getId());
-            
+
             rabbitTemplate.convertAndSend(FILA_ERRO_NOVO_CLIENTE, id_erro);
             rabbitTemplate.convertAndSend(FILA_ERRO_NOVO_CLIENTE_AUTENTICACAO, id_erro);
-            
+
+        }
+
+    }
+
+    @RabbitListener(queues = FILA_UPDATE_CLIENTE)
+    public void updateCliente(String msg) throws JsonMappingException, JsonProcessingException {
+        var cliente = objectMapper.readValue(msg, ClienteDTO.class);
+        try {
+            Long id = (cliente.getId());
+            if (id != null) {
+                var userFromDb = clienteRepository.findById(id);
+                userFromDb.get().setNome(cliente.getNome());
+                userFromDb.get().setSenha(cliente.getSenha());
+                userFromDb.get().setTelefone(cliente.getTelefone());
+                userFromDb.get().setEstado(cliente.getEstado());
+                userFromDb.get().setCidade(cliente.getCidade());
+                userFromDb.get().setCep(cliente.getCep());
+                userFromDb.get().setRua(cliente.getRua());
+                userFromDb.get().setNumero(cliente.getNumero());
+                userFromDb.get().setComplemento(cliente.getComplemento());
+                userFromDb.get().setAtivo(cliente.isAtivo());
+                clienteRepository.save(userFromDb.get());
+
+                // clienteRepository.;
+                System.out.println("Salvo (" + cliente.getNome() + ") " + msg);
+            }
+
+        } catch (Exception e) {
+            // ROLLBACK
+            System.out.println("Erro ao salvar cliente (" + cliente.getNome() + ") " + msg);
+            var id_erro = objectMapper.writeValueAsString(cliente.getId());
+
+            rabbitTemplate.convertAndSend(FILA_ERRO_NOVO_CLIENTE, id_erro);
+            rabbitTemplate.convertAndSend(FILA_ERRO_NOVO_CLIENTE_AUTENTICACAO, id_erro);
+
         }
 
     }
